f0c975b81417718e7a847624629cd3f8a3dd729f
rphoon <49342053+rphoon@users.noreply.github.com>
2021-06-07 23:15:36 +1000

    Update scale_deploy.ps1

--- scale_deploy.ps1
+++ scale_deploy.ps1
@@ -1,9 +1,36 @@
-# Define Variables
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $subscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string] $servicePrincipalClientId,
+    [Parameter(Mandatory = $true)]
+    [string] $servicePrincipalSecret,
+    [Parameter(Mandatory = $true)]
+    [string] $tenantId,
+    [Parameter(Mandatory = $true)]
+    [string] $resourceGroup,
+    [Parameter(Mandatory = $true)]
+    [string] $location,
+    [Parameter(Mandatory = $true)]
+    [string] $vCenterAddress,
+    [Parameter(Mandatory = $true)]
+    [string] $vCenterUser,
+    [Parameter(Mandatory = $true)]
+    [string] $vCenterPassword,
+    [Parameter(Mandatory = $true)]
+    [string] $srcPath,
+    [Parameter(Mandatory = $true)]
+    [string] $VMFolder,
+    [Parameter(Mandatory = $true)]
+    [string] $OSAdmin,
+    [Parameter(Mandatory = $true)]
+    [string] $OSPassword
+)
+
 # Connect to VMware vCenter
-Invoke-Expression ".\vars.ps1"
 Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false
-Connect-VIServer -Server $env:vCenterAddress -User $env:vCenterUser -Password $env:vCenterUserPassword -Force # vars are defined in vars.ps1
-$VMs = Get-Folder -Name $env:VMFolder | Get-VM # VMFolder is defined in vars.ps1
+Connect-VIServer -Server $vCenterAddress -User $vCenterUser -Password $vCenterPassword -Force # vars are defined in vars.ps1
+$VMs = Get-Folder -Name $VMFolder | Get-VM # VMFolder is defined in vars.ps1
 
 ForEach ($VMName in $VMs) {
   $VM = Get-VM $VMName
@@ -15,13 +42,13 @@ ForEach ($VMName in $VMs) {
   $Fullpath1 = $SrcPath + $File1 # SrcPath is defined in vars.ps1
   $Fullpath2 = $SrcPath + $File2 # SrcPath is defined in vars.ps1
 
-  Copy-VMGuestFile -VM $VM -Source $Fullpath1 -Destination $DstPath -LocalToGuest -GuestUser $env:OSAdmin -GuestPassword $env:OSAdminPassword -Force
-  Copy-VMGuestFile -VM $VM -Source $Fullpath2 -Destination $DstPath -LocalToGuest -GuestUser $env:OSAdmin -GuestPassword $env:OSAdminPassword -Force
+  Copy-VMGuestFile -VM $VM -Source $Fullpath1 -Destination $DstPath -LocalToGuest -GuestUser $OSAdmin -GuestPassword $OSPassword -Force
+  Copy-VMGuestFile -VM $VM -Source $Fullpath2 -Destination $DstPath -LocalToGuest -GuestUser $OSAdmin -GuestPassword $OSPassword -Force
 
   # Onboarding VM to Azure Arc
   $Command = $DstPath + $File1
   Write-Host "Hold tight, I am onboarding $VMName Virtual Machine to Azure Arc..." -ForegroundColor Cyan 
-  $Result = Invoke-VMScript -VM $VM -ScriptText $Command -GuestUser $env:OSAdmin -GuestPassword $env:OSAdminPassword
+  $Result = Invoke-VMScript -VM $VM -ScriptText $Command -GuestUser $OSAdmin -GuestPassword $OSPassword
   $ExitCode = $Result.ExitCode
   if ($ExitCode = "0") {
     Write-Host $VMName is now successfully onboarded to Azure Arc -ForegroundColor Green
@@ -29,5 +56,5 @@ ForEach ($VMName in $VMs) {
   Else {
     Write-Host $VMName returned exit code $ExitCode -ForegroundColor Red
   }
-  $Delete = Invoke-VMScript -VM $VM -ScriptText "Remove-Item -Force -Recurse -Path $DstPath" -GuestUser $env:OSAdmin -GuestPassword $env:OSAdminPassword
+  $Delete = Invoke-VMScript -VM $VM -ScriptText "Remove-Item -Force -Recurse -Path $DstPath" -GuestUser $OSAdmin -GuestPassword $OSPassword
 }
