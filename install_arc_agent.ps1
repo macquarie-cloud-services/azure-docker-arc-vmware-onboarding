fee19dff331cadacdf0093d79f9aa9631e113b3a
rphoon <49342053+rphoon@users.noreply.github.com>
2021-06-10 18:43:54 +1000

    Update install_arc_agent.ps1
    Changed var to parameterers.

--- install_arc_agent.ps1
+++ install_arc_agent.ps1
@@ -1,5 +1,17 @@
-# Injecting environment variables
-Invoke-Expression "C:\arctemp\vars.ps1"
+param(
+    [Parameter(Mandatory = $true)]
+    [string] $subscriptionId,
+    [Parameter(Mandatory = $true)]
+    [string] $servicePrincipalClientId,
+    [Parameter(Mandatory = $true)]
+    [string] $servicePrincipalSecret,
+    [Parameter(Mandatory = $true)]
+    [string] $tenantId,
+    [Parameter(Mandatory = $true)]
+    [string] $resourceGroup,
+    [Parameter(Mandatory = $true)]
+    [string] $location
+)
 
 # Download the package
 function download() {$ProgressPreference="SilentlyContinue"; Invoke-WebRequest -Uri https://aka.ms/AzureConnectedMachineAgent -OutFile AzureConnectedMachineAgent.msi}
@@ -14,12 +26,12 @@ if($exitCode -ne 0) {
 
 # Run connect command
 & "$env:ProgramW6432\AzureConnectedMachineAgent\azcmagent.exe" connect `
-  --service-principal-id $env:servicePrincipalClientId `
-  --service-principal-secret $env:servicePrincipalSecret `
-  --resource-group $env:resourceGroup `
-  --tenant-id $env:tenant_id `
-  --location $env:location `
-  --subscription-id $env:subscription_id `
+  --service-principal-id $servicePrincipalClientId `
+  --service-principal-secret $servicePrincipalSecret `
+  --resource-group $resourceGroup `
+  --tenant-id $tenantId `
+  --location $location `
+  --subscription-id $subscriptionId `
   --cloud "AzureCloud" `
   --tags "Project=jumpstart_azure_arc_servers" `
   --correlation-id "d009f5dd-dba8-4ac7-bac9-b54ef3a6671a"
