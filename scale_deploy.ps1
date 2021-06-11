99d730761cc52716220fd52d2815d7bf42f4ea4a
rphoon <49342053+rphoon@users.noreply.github.com>
2021-06-11 23:32:15 +1000

    Update scale_deploy.ps1

--- scale_deploy.ps1
+++ scale_deploy.ps1
@@ -32,25 +32,53 @@ $VMs = Get-Folder -Name $VMFolder | Get-VM
 
 ForEach ($VMName in $VMs) {
   $VM = Get-VM $VMName
+  If ($VM.GuestId -match "windows") {
+    # Define scripts information
+    $File1 = "install_arc_agent.ps1"
+    $srcPath = "C:\arcOnboarding\"
+    $DstPath = "C:\arctemp\"
+    $Fullpath1 = $srcPath + $File1
 
-  # Define scripts information
-  $File1 = "install_arc_agent.ps1"
-  $srcPath = "C:\arcOnboarding\"
-  $DstPath = "C:\arctemp\"
-  $Fullpath1 = $srcPath + $File1
+    Copy-VMGuestFile -VM $VM -Source ".\install_arc_agent.ps1" -Destination $DstPath -LocalToGuest -GuestUser $OSAdmin -GuestPassword $OSPassword -Force
 
-  Copy-VMGuestFile -VM $VM -Source ".\install_arc_agent.ps1" -Destination $DstPath -LocalToGuest -GuestUser $OSAdmin -GuestPassword $OSPassword -Force
-
-  # Onboarding VM to Azure Arc
-  $Command = $DstPath + $File1 + " -servicePrincipalClientId $servicePrincipalClientId -servicePrincipalSecret $servicePrincipalSecret -resourceGroup $resourceGroup -tenantId $tenantId -location $location -subscriptionId $subscriptionId"
-  Write-Output "`nOnboarding $VMName Virtual Machine to Azure Arc..." -ForegroundColor Cyan 
-  $Result = Invoke-VMScript -VM $VM -ScriptText $Command -GuestUser $OSAdmin -GuestPassword $OSPassword
-  $ExitCode = $Result.ExitCode
-  if ($ExitCode = "0") {
-    Write-Output $VMName is now successfully onboarded to Azure Arc -ForegroundColor Green
+    # Onboarding VM to Azure Arc
+    $Command = $DstPath + $File1 + " -servicePrincipalClientId $servicePrincipalClientId -servicePrincipalSecret $servicePrincipalSecret -resourceGroup $resourceGroup -tenantId $tenantId -location $location -subscriptionId $subscriptionId"
+    Write-Output "`nOnboarding $VMName Virtual Machine to Azure Arc..." -ForegroundColor Cyan 
+    $Result = Invoke-VMScript -VM $VM -ScriptText $Command -GuestUser $OSAdmin -GuestPassword $OSPassword
+    $ExitCode = $Result.ExitCode
+    if ($ExitCode = "0") {
+    Write-Output "$VMName is now successfully onboarded to Azure Arc"
+    }
+    Else {
+    Write-Output "$VMName returned exit code $ExitCode"
+    }
+    $Delete = Invoke-VMScript -VM $VM -ScriptText "Remove-Item -Force -Recurse -Path $DstPath" -GuestUser $OSAdmin -GuestPassword $OSPassword
   }
   Else {
-    Write-Output $VMName returned exit code $ExitCode -ForegroundColor Red
+    $File2 = "install_arc_agent.sh"
+    $sCopy2 = @{
+        Source = $srcPath + $File2
+        Destination = "/tmp/arctemp/"
+        VM = $VM
+        LocalToGuest = $true
+        GuestUser = "$OSAdmin"
+        GuestPassword = "$OSPassword"
+        Verbose = $false
+    }
+
+    # Onboarding VM to Azure Arc
+    Write-Output "`nOnboarding $VMName Virtual Machine to Azure Arc..."
+    Copy-VMGuestFile @sCopy2 -Force
+    $Result = Invoke-VMScript -VM $VM -ScriptText "sudo bash /tmp/arctemp/install_arc_agent.sh" -GuestUser $OSAdmin -GuestPassword $OSPassword
+    $ExitCode = $Result.ExitCode
+    if ($ExitCode = "0") {
+        Write-Output "$VMName is now successfully onboarded to Azure Arc"
+    }
+    Else {
+        Write-Output "$VMName returned exit code $ExitCode"
+    }
+
+    # Cleaning garbage
+    $Delete = Invoke-VMScript -VM $VM -ScriptText "rm -rf /tmp/arctemp/" -GuestUser $OSAdmin -GuestPassword $OSPassword
   }
-  $Delete = Invoke-VMScript -VM $VM -ScriptText "Remove-Item -Force -Recurse -Path $DstPath" -GuestUser $OSAdmin -GuestPassword $OSPassword
 }
